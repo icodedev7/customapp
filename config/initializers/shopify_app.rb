989862c954979afbc558adc0ce0a02b77309c210
icodedev7 <icode.dev7@gmail.com>
2016-01-06 13:18:47 +0530

    Create shopify_app.rb

---
+++ config/initializers/shopify_app.rb
@@ -0,0 +1,7 @@
+ShopifyApp.configure do|config|
+config.api_key="9274b5fb757eea9d28190e00ec188a80"
+config.secret="a42c103cbe0534285b7d2b31c02b364c"
+config.redirect_uri="https://customappp.herokuapp.com/auth/shopify/callback"
+config.scope="read_orders, read_products"
+config.embedded_app=true
+end
