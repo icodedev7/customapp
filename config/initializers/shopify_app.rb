a303c84e5611c1022bd01e8095d2329dd654590e
amrita pritam <icode.dev7@gmail.com>
2016-01-07 15:11:29 +0530

    commit

--- config/initializers/shopify_app.rb
+++ config/initializers/shopify_app.rb
@@ -1,4 +1,4 @@
-ShopifyApp.configure do|config|
+customappp.configure do|config|
 config.api_key="9274b5fb757eea9d28190e00ec188a80"
 config.secret="a42c103cbe0534285b7d2b31c02b364c"
 config.redirect_uri="https://customappp.herokuapp.com/auth/shopify/callback"
