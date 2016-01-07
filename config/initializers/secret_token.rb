11dcaccc79e5fdc63c449e61aa44d573b4c87ab5
amrita pritam <icode.dev7@gmail.com>
2016-01-07 15:19:39 +0530

    commit

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,5 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-customappp::Application.config.secret_token = '86c3f3cff28759810ff1beca0ad515051d438537ce48a9bde6777ed0dcf869a1a17be893d0456839423c5030057a5c57710173705b12791afea0fe7642c6e5c5'
-                                               
+EmbededApp::Application.config.secret_token = '86c3f3cff28759810ff1beca0ad515051d438537ce48a9bde6777ed0dcf869a1a17be893d0456839423c5030057a5c57710173705b12791afea0fe7642c6e5c5'
--- config/initializers/shopify_app.rb
+++ config/initializers/shopify_app.rb
@@ -1,4 +1,4 @@
-customappp.configure do|config|
+ShopifyApp.configure do|config|
 config.api_key="9274b5fb757eea9d28190e00ec188a80"
 config.secret="a42c103cbe0534285b7d2b31c02b364c"
 config.redirect_uri="https://customappp.herokuapp.com/auth/shopify/callback"
