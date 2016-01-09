3a5c4ad8d3dd49de693d44db1eeb172a1494085c
amrita pritam <icode.dev7@gmail.com>
2016-01-09 15:21:54 +0530

    commit

--- lib/generators/shopify_app/install/templates/shopify_app.rb
+++ lib/generators/shopify_app/install/templates/shopify_app.rb
@@ -1,7 +1,7 @@
 ShopifyApp.configure do |config|
-  config.api_key = "<%= opts[:api_key] %>"
-  config.secret = "<%= opts[:secret] %>"
-  config.redirect_uri = "<%= opts[:redirect_uri] %>"
-  config.scope = "<%= opts[:scope] %>"
+  config.api_key = "<%= opts[:d557fb57a6a882d152545284e02fbc31] %>"
+  config.secret = "<%= opts[:a0d5ba398f7eb2de0c9dc7672eb94b53] %>"
+  config.redirect_uri = "<%= opts[:https://customappp.herokuapp.com/auth/shopify/callback] %>"
+  config.scope = "<%= opts[:read_customers, read_orders, write_products] %>"
   config.embedded_app = <%= embedded_app? %>
 end
