6c38500b6a74fc93671ef576090983f821aaf0ef
icodedev7 <icode.dev7@gmail.com>
2016-01-09 13:25:19 +0530

    Update shopify_app.rb

--- test/dummy/config/initializers/shopify_app.rb
+++ test/dummy/config/initializers/shopify_app.rb
@@ -1,6 +1,6 @@
 ShopifyApp.configure do |config|
-  config.api_key = "key"
-  config.secret = "secret"
+  config.api_key = "9274b5fb757eea9d28190e00ec188a80"
+  config.secret = "a42c103cbe0534285b7d2b31c02b364c"
   config.scope = 'read_orders, read_products'
   config.embedded_app = true
 end
